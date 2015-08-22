544625406d9cc1e99e12d2bf704be4bca1839d61
Theron Burger <burger@meso.net>
2015-08-22 18:49:27 +0200

    working

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,22 +1,142 @@
 #include "ofApp.h"
 
+tableObject tableObjects::newTableObject(ofxCvColorImage CvImage, ofPoint pos, ofVec2f size){
+    tableObject myNewTableObject;
+    myNewTableObject.pos        = pos;
+    myNewTableObject.size       = size;
+    myNewTableObject.CvImage    = CvImage;
+    
+    return myNewTableObject;
+    
+}
+
+bool tableObjects::isNew(tableObject object){
+    
+    return true;
+    
+}
+
+bool tableObjects::add(ofxCvColorImage image, ofPoint pos,ofVec2f size){
+    tableObject myNewTableObject = newTableObject(image, pos, size);
+    
+    if (isNew(myNewTableObject)){
+        
+    }
+    
+    
+    
+}
+
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    vidGrabber.setVerbose(true);
+    vidGrabber.initGrabber(320,240);
+
+    
+    colorImg.allocate(320,240);
+    grayImage.allocate(320,240);
+    grayBg.allocate(320,240);
+    grayDiff.allocate(320,240);
+    
+    bLearnBakground = true;
+    threshold = 80;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    ofBackground(100,100,100);
+    
+    bool bNewFrame = false;
+    
+    vidGrabber.update();
+	   bNewFrame = vidGrabber.isFrameNew();
+    
+    if (bNewFrame){
+        colorImg.setFromPixels(vidGrabber.getPixels(), 320,240);
+        
+        grayImage = colorImg;
+        if (bLearnBakground == true){
+            grayBg = grayImage;		// the = sign copys the pixels from grayImage into grayBg (operator overloading)
+            bLearnBakground = false;
+        }
+        
+        // take the abs value of the difference between background and incoming and then threshold:
+        grayDiff.absDiff(grayBg, grayImage);
+        grayDiff.threshold(threshold);
+        
+        // find contours which are between the size of 20 pixels and 1/3 the w*h pixels.
+        // also, find holes is set to true so we will get interior contours as well....
+        contourFinder.findContours(grayDiff, 20, (340*240)/3, 10, true);	// find holes
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    
+    // draw the incoming, the grayscale, the bg and the thresholded difference
+    ofSetHexColor(0xffffff);
+    colorImg.draw(20,20);
+    grayImage.draw(360,20);
+    grayBg.draw(20,280);
+    grayDiff.draw(360,280);
+    
+    // then draw the contours:
+    
+    ofFill();
+    ofSetHexColor(0x333333);
+    ofRect(360,540,320,240);
+    ofSetHexColor(0xffffff);
+    
+    // we could draw the whole contour finder
+    //contourFinder.draw(360,540);
+    
+    // or, instead we can draw each blob individually from the blobs vector,
+    // this is how to get access to them:
+    for (int i = 0; i < contourFinder.nBlobs; i++){
+        contourFinder.blobs[i].draw(360,540);
+        
+        // draw over the centroid if the blob is a hole
+        ofSetColor(255);
+        if(contourFinder.blobs[i].hole){
+            ofDrawBitmapString("hole",
+                               contourFinder.blobs[i].boundingRect.getCenter().x + 360,
+                               contourFinder.blobs[i].boundingRect.getCenter().y + 540);
+        }
+    }
+    
+    
+    //attempt to find smallestBounds
+    
+    
+    
+    
+    // finally, a report:
+    ofSetHexColor(0xffffff);
+    stringstream reportStr;
+    reportStr << "bg subtraction and blob detection" << endl
+    << "press ' ' to capture bg" << endl
+    << "threshold " << threshold << " (press: +/-)" << endl
+    << "num blobs found " << contourFinder.nBlobs << ", fps: " << ofGetFrameRate();
+    ofDrawBitmapString(reportStr.str(), 20, 600);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    
+    switch (key){
+        case ' ':
+            bLearnBakground = true;
+            break;
+        case '+':
+            threshold ++;
+            if (threshold > 255) threshold = 255;
+            break;
+        case '-':
+            threshold --;
+            if (threshold < 0) threshold = 0;
+            break;
+    }
 
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,33 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
+
+struct tableObject {
+    ofImage             image;
+    ofxCvColorImage     CvImage;
+    ofPoint             pos;
+    ofVec2f             size;
+    bool                alive;
+    ofPath              path;
+
+    
+    
+};
+
+class tableObjects {
+
+    
+    vector<tableObject>  objects;
+    
+    bool isNew(tableObject object);
+    bool add(ofxCvColorImage image, ofPoint pos, ofVec2f size);
+    tableObject newTableObject(ofxCvColorImage CvImage, ofPoint pos, ofVec2f size);
+    
+    
+};
+
+
 
 class ofApp : public ofBaseApp{
 
@@ -18,5 +45,23 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+    
+        tableObjects            myTableObjects;
+    
+        ofVideoGrabber 		vidGrabber;
+    
+        ofxCvColorImage			colorImg;
+    
+        ofxCvGrayscaleImage 	grayImage;
+        ofxCvGrayscaleImage 	grayBg;
+        ofxCvGrayscaleImage 	grayDiff;
+    
+        ofxCvContourFinder 	contourFinder;
+    
+        int 				threshold;
+        bool				bLearnBakground;
+    
+    
 		
 };
